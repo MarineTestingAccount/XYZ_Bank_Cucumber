5a6cc27bf56b1d0b138bbd8ebaf42c175a837d4c
javaAutoTest <marine.melk.qa@mail.ru>
2022-06-30 12:48:20 +0400

    with assertions

--- src/test/java/stepsDefinitions/Hooks.java
+++ src/test/java/stepsDefinitions/Hooks.java
@@ -2,6 +2,7 @@ package stepsDefinitions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,6 +13,10 @@ import pages.LoginPage;
 import pages.ManagerPage;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import stepsDefinitions.TestAssertions;
 
 import static pages.BaseInfo.*;
 
@@ -21,6 +26,7 @@ public class Hooks {
         ManagerPage managerPage;
         AddCustomerPage addCustomerPage;
         ListCustomersPage listCustomersPage;
+
 
 
         @BeforeEach
@@ -36,30 +42,34 @@ public class Hooks {
             managerPage = new ManagerPage(driver);
             addCustomerPage = new AddCustomerPage(driver);
             listCustomersPage = new ListCustomersPage(driver);
+
+
         }
 
         @Test
         public void testDemo() throws InterruptedException {
             System.out.println(driver.getCurrentUrl());
+            List<String> userInfo = new ArrayList<>();
             loginPage.clickOnTheLoginBtn();
             loginPage.navigateToManagerPage();
             managerPage.clickOnAddCustomerBtn();
             managerPage.navigateToAddCustomerPage();
-            addCustomerPage.setFirstName("NewUser");
-            addCustomerPage.setLastName("lastUser");
-            addCustomerPage.setPostCode("123123");
+            userInfo.addAll(Arrays.asList("NewUser","lastUser","123123"));
+            addCustomerPage.setFirstName(userInfo.get(0));
+            addCustomerPage.setLastName(userInfo.get(1));
+            addCustomerPage.setPostCode(userInfo.get(2));
             addCustomerPage.clickOnSubmit();
             addCustomerPage.acceptPopUp();
+
             addCustomerPage.clickOnCustomersBtn();
             listCustomersPage.searchCustomer("NewUser");
             listCustomersPage.getAddedCustomer();
+           // Assert.assertEquals(userInfo,listCustomersPage.getAddedCustomer());
+           TestAssertions.assertAddedCustomerInfo(userInfo,listCustomersPage.getAddedCustomer());
             listCustomersPage.deleteUser();
-
-
-
-
+            //Assert.assertNotEquals(userInfo,listCustomersPage.getAddedCustomer());
+            TestAssertions.assertDeletedCustomerInfo(userInfo,listCustomersPage.getAddedCustomer());
         }
-
     @After
     public void cleanUp(){
         driver.manage().deleteAllCookies();
--- src/test/java/stepsDefinitions/TestAssertions.java
+++ src/test/java/stepsDefinitions/TestAssertions.java
@@ -1,4 +1,19 @@
 package stepsDefinitions;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import pages.ListCustomersPage.*;
+import  pages.AddCustomerPage.*;
+
+import java.util.List;
+
 public class TestAssertions {
+    //Assert that customer is added with correct info
+    public static void assertAddedCustomerInfo(List<String> expectedList, List<String> actualList) {
+        Assert.assertEquals(expectedList, actualList);
+    }
+    public static void assertDeletedCustomerInfo(List<String> expectedList, List<String> actualList) {
+        Assert.assertNotEquals(expectedList, actualList);
+    }
+
 }
